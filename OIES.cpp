381ef51215b806b09644a38203cc93b808515908
Saubhagya Ji Jaiswal <sjijaiswal4@gmail.com>
2026-04-04 23:12:54 +0530

    added

---
+++ OIES.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> vec(n);
+        vec[0]=1;
+        for(int i=1;i<n;i++){
+            vec[i]=(i+1)*vec[i-1];
+        }
+        for(int i=0;i<n;i++){
+            cout<<vec[i]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
