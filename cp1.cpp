d82defe6ed20edd330511e9bb666334bef1f5e08
Saubhagya Ji Jaiswal <sjijaiswal4@gmail.com>
2026-05-16 23:35:26 +0530

    solved

---
+++ cp1.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        int cnt0 = 0, cnt1 = 0, cnt2 = 0;
+
+        for (int i = 0; i < n; i++) {
+            int x;
+            cin >> x;
+
+            if (x == 0) cnt0++;
+            else if (x == 1) cnt1++;
+            else cnt2++;
+        }
+
+        int pairs = min(cnt1, cnt2);
+
+        cnt1 -= pairs;
+        cnt2 -= pairs;
+
+        int ans = cnt0 + pairs + cnt1 / 3 + cnt2 / 3;
+
+        cout << ans << endl;
+    }
+
+    return 0;
+}
